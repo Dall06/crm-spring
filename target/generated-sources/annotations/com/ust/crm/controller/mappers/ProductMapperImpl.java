3cd7e846441c249b7eb6b068952bffedc4dc9e2e
Dall06 <devleon.06@gmail.com>
2022-09-12 14:55:18 -0500

    change: test new docker commands

--- target/generated-sources/annotations/com/ust/crm/controller/mappers/ProductMapperImpl.java
+++ target/generated-sources/annotations/com/ust/crm/controller/mappers/ProductMapperImpl.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-09-12T12:41:36-0500",
-    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 17.0.4.1 (Amazon.com Inc.)"
+    date = "2022-09-12T13:37:55-0500",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 11.0.2 (Oracle Corporation)"
 )
 @Component
 public class ProductMapperImpl implements ProductMapper {
